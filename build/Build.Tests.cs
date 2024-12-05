16b9f892f06017e7c8664780cfce4092cd1fd084
Phil Magalios <pmagalios@drivecentric.com>
2024-12-05 11:07:56 -0600

    Revert "Removing Fedora from our builds (#1050)"
    This reverts commit 266b08874a9c5aa7f639f25be50483542f10634d.

--- build/Build.Tests.cs
+++ build/Build.Tests.cs
@@ -99,6 +99,7 @@ partial class Build
                 new TestConfigurationOnLinuxDistribution(NetCore, "linux-x64", "ubuntu:focal", "deb"), // 20.04
                 new TestConfigurationOnLinuxDistribution(NetCore, "linux-x64", "ubuntu:bionic", "deb"), // 18.04
                 new TestConfigurationOnLinuxDistribution(NetCore, "linux-x64", "ubuntu:xenial", "deb"), // 16.04
+                new TestConfigurationOnLinuxDistribution(NetCore, "linux-x64", "fedora:39", "rpm"),
                 new TestConfigurationOnLinuxDistribution(NetCore, "linux-x64", "roboxes/rhel8", "rpm"),
             };
             
